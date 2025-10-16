803f1d69aaa5fec9a8dae76db6d196a9ee3d298b
rigerc <bond@bsgb.nl>
2025-10-16 04:52:02 +0200

    asd

---
+++ run_once_after_install-02-finished.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euo pipefail
+
+source "$HOME/.bashrc"
+test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# Colors for output
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+echo -e "${RED}Restart shell to finish...${NC}"
