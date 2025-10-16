6e82ed34905a9c753f112cfc105572f85dd83852
rigerc <bond@bsgb.nl>
2025-10-17 00:06:50 +0200

    asd

---
+++ .install-bitwarden.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# exit immediately if password-manager-binary is already in $PATH asd
+type bw >/dev/null 2>&1 && exit
+
+case "$(uname -s)" in
+Darwin)
+    # commands to install password-manager-binary on Darwin
+    ;;
+Linux)
+    sudo pacman -S --quiet --noconfirm bitwarden-cli
+    export BW_SESSION=$(bw login && bw unlock --raw) 
+    ;;
+*)
+    echo "unsupported OS"
+    exit 1
+    ;;
+esac
