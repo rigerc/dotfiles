6d1e49d3cd0f89ceff380f68352ebc910f583205
rigerc <bond@bsgb.nl>
2025-10-17 20:35:09 +0200

    fix

--- .install-gopass.sh
+++ .install-gopass.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
 mkdir -p ".local/share/chezmoi/.tmp"
-
-case "$(uname -s)" in
-Darwin)
-    # commands to install password-manager-binary on Darwin
-    ;;
-Linux)
-# ...
-    ;;
-*)
-    echo "unsupported OS"
-    exit 1
-    ;;
-esac
