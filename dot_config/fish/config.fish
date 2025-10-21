c20cef9e206800a61228b36a777f55865bf81d65
rigerc <bond@bsgb.nl>
2025-10-21 16:58:21 +0200

    asdasd

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -4,14 +4,29 @@ if status is-interactive
 end
 
 # shared .env file zsh/fish
-function setenv
-    if [ $argv[1] = PATH ]
-        # Replace colons and spaces with newlines
-        set -gx PATH (echo $argv[2] | tr ': ' \n)
-    else
-        set -gx $argv
-    end
- end
+egrep "^export " ~/.env | while read e
+	set var (echo $e | sed -E "s/^export ([A-Z_]+)=(.*)\$/\1/")
+	set value (echo $e | sed -E "s/^export ([A-Z_]+)=(.*)\$/\2/")
+	
+	# remove surrounding quotes if existing
+	set value (echo $value | sed -E "s/^\"(.*)\"\$/\1/")
+
+	if test $var = "PATH"
+		# replace ":" by spaces. this is how PATH looks for Fish
+		set value (echo $value | sed -E "s/:/ /g")
+	
+		# use eval because we need to expand the value
+		eval set -xg $var $value
+
+		continue
+	end
+
+	# evaluate variables. we can use eval because we most likely just used "$var"
+	set value (eval echo $value)
+
+	#echo "set -xg '$var' '$value' (via '$e')"
+	set -xg $var $value
+end
 
 source ~/.env
 
