b0885fea30bdf8095c6d6d92cdfd50ed6bc2182c
rigerc <bond@bsgb.nl>
2025-10-25 20:13:04 +0200

    chore: update chezmoi aliases and pwd.fish
    - Fix chezmoi-go alias syntax
    - Replace complex pwd.fish logic with onefetch
    - Remove unused GUM_FORMAT_THEME setting
    - Simplify directory change handler

--- dot_config/fish/conf.d/pwd.fish
+++ dot_config/fish/conf.d/pwd.fish
@@ -7,25 +7,9 @@ status is-interactive; or exit
 # --on-variable is a fish builtin that changes whenever the directory changes
 # so this function will run whenever the directory changes
 function auto_pwd --on-variable PWD
-    set -x GUM_FORMAT_THEME dark
 
     # check if .git/ exists and is a git repo and if onefetch is installed
     if test -d .git && git rev-parse --git-dir >/dev/null 2>&1
-        # readme file
-        if test -f README.md
-            awk '/^##/{exit} 1' README.md | string trim \
-                | gum format | grep -v 'Image: image' 2>&1 | head -20
-        end
-
-        # recent commits
-        echo -e "## Recent Activity\n" | gum format
-        git l -10 \
-            --since='1 week ago' \
-            | devmoji --log --color \
-            | sed 's/^/  /'
-
-        # local changes
-        echo -e "## Status\n" | gum format
-        git -c color.ui=always st | sed 's/^/  /'
+        onefetch
     end
 end
